d0c0c7955d0c9eff770b5589c15ee72cf078807d
Usuario <Usuario@10.22.148.97>
2018-09-13 11:55:42 +0100

    Kata1 V4

--- src/kata1/Kata1.java
+++ src/kata1/Kata1.java
@@ -1,14 +1,12 @@
 package kata1;
 
-import java.util.Calendar;
-import java.util.GregorianCalendar;
+import java.time.LocalDate;
 
 public class Kata1 {
 
     public static void main(String[] args) {
-        Calendar date;
-        date = GregorianCalendar.getInstance();
-        date.set(1997, 11, 9);
+        LocalDate date;
+        date = LocalDate.of(1997, 11, 9);
         
         Person person = new Person("Adrián", date);
         System.out.println(person.getName() + " tiene " + person.getAge() + " años");
